fd3501fcbd8e76e3545edc816704252f3cc3e161
mudbile <eulerspill@protonmail.com>
2018-10-06 15:25:33 +1000

    Recipt object (no location yet)

--- app/src/main/java/au/edu/usc/dcd008/myreceipts/Receipt.java
+++ app/src/main/java/au/edu/usc/dcd008/myreceipts/Receipt.java
@@ -4,61 +4,59 @@ import java.util.Date;
 import java.util.UUID;
 
 public class Receipt {
+        private UUID mId;
+        private String mTitle;
+        private String mShopName;
+        private String mComment;
+        private Date mDate;
+        //missing: location
 
-    private UUID mId;
-    private String mTitle;
-    private String mSuspect;
-    private Date mDate;
-    private boolean mSolved;
+        public Receipt(){
+            this(UUID.randomUUID());
+        }
 
-    public String getPhotoFilename(){
-        return "IMG_" + getId().toString() + ".jpg";
+        public Receipt(UUID uuid){
+            mId = uuid;
+            mDate = new Date();
+        }
+
+        public String getPhotoFilename(){
+            return "IMG_" + getId().toString() + ".jpg";
+        }
+
+        public UUID getId() {
+            return mId;
+        }
+
+        public String getTitle() {
+            return mTitle;
+        }
+
+        public void setTitle(String title) {
+            mTitle = title;
+        }
+
+        public Date getDate() {
+            return mDate;
+        }
+
+        public void setDate(Date date) {
+            mDate = date;
+        }
+
+        public String getShopName() {
+            return mShopName;
+        }
+
+        public void setShopName(String shopName) {
+            mShopName = shopName;
+        }
+
+        public String getComment() {
+            return mComment;
+        }
+
+        public void setComment(String comment) {
+            mComment = comment;
+        }
     }
-
-    public UUID getId() {
-        return mId;
-    }
-
-    public String getSuspect() {
-        return mSuspect;
-    }
-
-    public void setSuspect(String suspect) {
-        mSuspect = suspect;
-    }
-
-    public String getTitle() {
-        return mTitle;
-    }
-
-    public void setTitle(String title) {
-        mTitle = title;
-    }
-
-    public Date getDate() {
-        return mDate;
-    }
-
-    public void setDate(Date date) {
-        mDate = date;
-    }
-
-    public boolean isSolved() {
-        return mSolved;
-    }
-
-    public void setSolved(boolean solved) {
-        mSolved = solved;
-    }
-
-    public Receipt(){
-        this(UUID.randomUUID());
-    }
-
-    public Receipt(UUID uuid){
-        mId = uuid;
-        mDate = new Date();
-    }
-
-
-}
